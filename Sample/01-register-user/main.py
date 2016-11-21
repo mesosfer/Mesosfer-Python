8759617353aaf27aed25ca24625e91fc1234d906
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2016-11-21 18:12:12 +0700

    update login & register

--- Sample/01-register-user/main.py
+++ Sample/01-register-user/main.py
@@ -5,6 +5,6 @@ data = {"firstname": "John", "lastname": "Doe", "email": "john.doe@gmail.com", "
 
 resp = requests.post('https://api.mesosfer.com/api/v2/users',
 	 json=data,
-	 headers={'Content-Type':'application/json', 'Authorization':'Bearer ACCESS_TOKEN', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
+	 headers={'Content-Type':'application/json', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
 
 print(resp.json())
--- Sample/02-login-user/main.py
+++ Sample/02-login-user/main.py
@@ -5,6 +5,6 @@ data = {"email": "john.doe@gmail.com", "password": "johndoe"}
 
 resp = requests.post('https://api.mesosfer.com/api/v2/users/signin',
 	 json=data,
-	 headers={'Content-Type':'application/json', 'Authorization':'Bearer ACCESS_TOKEN', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
+	 headers={'Content-Type':'application/json', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
 
 print(resp.json())
