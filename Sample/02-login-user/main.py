dd3e2bc70537ebc85647c9645e8cad8ea23d9e34
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2016-12-10 10:30:59 +0700

    Update Paramaeter Format

--- Sample/02-login-user/main.py
+++ Sample/02-login-user/main.py
@@ -1,10 +1,10 @@
-import requests
+import requests, json
 
 #### LOGIN USER
 data = {"email": "john.doe@gmail.com", "password": "johndoe"}
 
 resp = requests.post('https://api.mesosfer.com/api/v2/users/signin',
-	 json=data,
+	 data=json.dumps(data),
 	 headers={'Content-Type':'application/json', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
 
 print(resp.json())
--- Sample/06-update-data/main.py
+++ Sample/06-update-data/main.py
@@ -1,10 +1,10 @@
-import requests
+import requests, json
 
 data = {"metadata":{"uuid": "cb10023fa31833944199a8730c7c1aec", "major": 888, "minor": 888, "isEnterState": True, "isActive": True}}
 
 ## UPDATE a DATA
 resp = requests.put('https://api.mesosfer.com/api/v2/data/bucket/Attendance/OBJECT_ID',
-	 json=data,
+	 data=json.dumps(data),
      headers={'Content-Type':'application/json', 'Authorization':'Bearer ACCESS_TOKEN', 'X-Mesosfer-AppId':'APP_ID', 'X-Mesosfer-AppKey':'APP_KEY'})
 
 print(resp.json())
